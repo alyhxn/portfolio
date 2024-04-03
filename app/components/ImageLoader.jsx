7b3ed0dec23ac67dc7b3ffc6e0cc2eff4bdc7280
Ali Hassan <hassanali8812@gmail.com>
2024-04-03 09:38:38 +0500

    Added more responsiveness

--- app/components/ImageLoader.jsx
+++ app/components/ImageLoader.jsx
@@ -6,15 +6,16 @@ import Image from 'next/image'
 const ImageLoader = ({ className, src, alt, width, height }) => {
   const [loaded, setLoaded] = useState(false);
   const styles = {
-    minWidth: width,
+    maxWidth: width,
     height,
-    overflow: 'hidden'
+    overflow: 'hidden',
+    width: '100%'
   }
 
   return (
-    <div className='relative'>
+    <div style={styles} className='relative'>
       <Image onLoad={() => setLoaded(true)} priority className={`${className} ${loaded ? 'block' : 'hidden'}`} src={src} alt={alt} width={width} height={height}/>
-      <div className={`${className} absolute top-0 img-overlay ${loaded && 'opacity-0'}`}></div>
+      <div style={styles} className={`${className} absolute top-0 img-overlay ${loaded && 'opacity-0'}`}></div>
     </div>
   )
 }
